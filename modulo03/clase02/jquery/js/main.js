502d9d8be9a2c38a3faee15d75c32bc4a674c797
jtorriccelli1986 <jtorriccelli1986@gmail.com>
2018-04-05 22:01:20 -0500

    update

---
+++ modulo03/clase02/jquery/js/main.js
@@ -0,0 +1,40 @@
+/*$("#inicio-menu").click(tarea1);
+function tarea1(){
+	eliminarActivo();
+	$("#inicio-menu").addClass("activo");
+}
+
+
+$("#nosotros-menu").click(tarea2);
+function tarea2(){
+	eliminarActivo();
+	$("#nosotros-menu").addClass("activo");
+}
+
+
+function eliminarActivo(){
+$(".nav-site ul li").removeClass("activo");
+}
+*/
+
+
+
+
+
+
+/*$(".nav-site ul li").click(tarea)
+function tarea(){
+	$(".nav-site ul li").removeClass("activo");
+  $(this).addClass("activo");
+}*/
+
+
+$(".nav-site ul li").click(function(){
+	$(".nav-site ul li").removeClass("activo");
+  $(this).addClass("activo");
+})
+
+
+
+
+
