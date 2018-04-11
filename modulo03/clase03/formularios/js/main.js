1c7a108d44c7ef455e7f9291254fd81e8e4b7229
jtorriccelli1986 <jtorriccelli1986@gmail.com>
2018-04-10 20:50:57 -0500

    update

---
+++ modulo03/clase03/formularios/js/main.js
@@ -0,0 +1,11 @@
+$(".btn-enviar").click(function(){
+	// anula el evento por defecto
+	event.preventDefault();
+
+	var nombres=$("#inp-nombres").val();
+	var apellidos=$("#inp-apellidos").val();
+	var correo=$("#inp-correo").val();
+
+	
+	
+})
