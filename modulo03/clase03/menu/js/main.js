3c3457e53548081c5aa0127d9988da421c2b4c33
jtorriccelli1986 <jtorriccelli1986@gmail.com>
2018-04-10 20:24:42 -0500

    update

---
+++ modulo03/clase03/menu/js/main.js
@@ -0,0 +1,4 @@
+$(".ico-menu").click(function(){
+	//$(".lista-menu")	
+	$(".nav-site ul").slideToggle();
+})
---
+++ modulo03/clase03/menu/js/main2.js
@@ -0,0 +1,10 @@
+$(".ico-menu").click(function(){
+	//$(".lista-menu")	
+	//$(".modal-menu").show();
+	$(".modal-menu").fadeIn(500);
+})
+$(".close-modal").click(function(){
+	//$(".modal-menu").hide();
+	$(".modal-menu").fadeOut(1000);
+})
+
