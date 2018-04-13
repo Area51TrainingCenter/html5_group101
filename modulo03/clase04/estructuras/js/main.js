c9eaee9be407d00434f7b9d23ee5dba8a234d73c
jtorriccelli1986 <jtorriccelli1986@gmail.com>
2018-04-12 20:01:06 -0500

    update

--- modulo03/clase04/estructuras/js/main.js
+++ modulo03/clase04/estructuras/js/main.js
@@ -1,6 +1,28 @@
 $("#accion_sumar").click(function(){
+	//detengo el comporatamiento por defecto
 	event.preventDefault();
+	
+	var num1;
+	var num2;
+	var suma;
+	num1=$("#inp_num1").val();
+	num2=$("#inp_num2").val();
+	
+	suma=parseInt(num1)+parseInt(num2);
+	
 
-	alert("click");
+// quiero saber si el 1er numero es
+// mayor que el segundo
+	
+	if(num1>num2){
+		$(".resultado").text(suma);
+		// si la condicion resuelve verdad
+		
+	}
+	else{
+			$(".resultado").text("el num1 no puede ser menor que el numer 2");
+		// si la condicion resuelve falso
+	}
+
 
 })
