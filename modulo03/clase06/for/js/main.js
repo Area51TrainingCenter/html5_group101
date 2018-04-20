c1d660582968079021aff44ac1d9926295c520f7
jtorriccelli1986 <jtorriccelli1986@gmail.com>
2018-04-19 22:03:32 -0500

    update

---
+++ modulo03/clase06/for/js/main.js
@@ -0,0 +1,11 @@
+/*
+for(valor incial,condicion,accion)
+{
+
+}*/
+var nombres=["martin","gian"];
+var limite=nombres.length;
+for(var i=0;i<limite;i++){
+	console.log(nombres[i]);
+
+}
--- modulo03/clase06/json/js/main.js
+++ modulo03/clase06/json/js/main.js
@@ -2,15 +2,112 @@ $(window).load(function(){
 
 		//var servicio="http://jsonplaceholder.typicode.com/users";
  		//$.getJSON(url,function)
- 		var servicio2="http://sminnova.com/restapitrujillo/sitiosturisticos";
+ 		// var servicio2="";
  		$.getJSON(servicio2,function(data){
- 			var nombre=data[0].nombre;
- 			var direccion=data[0].direccion;
- 			var tipo=data[0].tipo_ingreso;
- 			
- 			$("#table-sitios tbody").append("<tr><td>"+nombre+"</td><td>"+direccion+"</td><td>"+tipo+"</td></tr>")
+ 			var limite=data.length;
+ 			console.log(data);
+
+ 			for(var i=0;i<10;i++){
+				var nombre=data[i].nombre;
+ 				var direccion=data[i].direccion;
+ 				var tipo=data[i].tipo_ingreso;
+ 				var id=data[i].id;
+ 				//$("#table-sitios tbody").append("<tr><td>"+nombre+"</td><td>"+direccion+"</td><td>"+tipo+"</td></tr>")
+				
+				$("#table-sitios tbody").append(`
+ 						<tr>
+ 								<td>`+id+`</td>
+ 								<td>`+nombre+`</td>
+ 								<td>`+direccion+`</td>
+ 								<td>`+tipo+`</td>
+ 								<td> 
+ 									<button data-id-sitio="`+id+`" type="button" class="ver-sitio btn btn-success" onclick="traerDatos(this)">Ver</button>
+ 									<button data-id-sitio="`+id+`" type="button" class="btn btn-danger" onclick=borrarFila(this)>Borrar</button>
+								</td>
+ 						</tr>`)
+
+				}
+
+				/*$(".ver-sitio").click(function(){
+
+
+							$(".title-sitio").text("")
+		 						$(".img-sitio").attr("src","");
+		 						$(".contenido").html("");
+
+
+
+		 					var id=$(this).attr("data-id-sitio");
+		 					
+		 					
+		 					$.getJSON(url,function(data){
+		 						var nombre=data[0].nombre;
+		 						var foto=data[0].foto_principal;
+		 						var contenido=data[0].historia;
+
+		 						$(".title-sitio").text(nombre)
+		 						$(".img-sitio").attr("src",foto);
+		 						$(".contenido").html(contenido);
+		 					})
+
+
+		 		})*/
+
 
  		})
+ 	
+ 		/*$(".ver-sitio").click(function(){
+
+
+					$(".title-sitio").text("")
+ 						$(".img-sitio").attr("src","");
+ 						$(".contenido").html("");
+
+
+
+ 					var id=$(this).attr("data-id-sitio");
+ 					
+ 					var url="http://sminnova.com/restapitrujillo/sitiosturisticos/id/"+id;
+ 					$.getJSON(url,function(data){
+ 						var nombre=data[0].nombre;
+ 						var foto=data[0].foto_principal;
+ 						var contenido=data[0].historia;
+
+ 						$(".title-sitio").text(nombre)
+ 						$(".img-sitio").attr("src",foto);
+ 						$(".contenido").html(contenido);
+ 					})
+
+
+ 		})*/
 
 
 })
+
+
+	function traerDatos(ele){
+ 			var id=$(ele).attr("data-id-sitio");
+ 		
+
+ 							$(".title-sitio").text("")
+		 					$(".img-sitio").attr("src","");
+		 					$(".contenido").html("");
+
+		 					//var url=";
+		 					$.getJSON(url,function(data){
+		 						var nombre=data[0].nombre;
+		 						var foto=data[0].foto_principal;
+		 						var contenido=data[0].historia;
+
+		 						$(".title-sitio").text(nombre)
+		 						$(".img-sitio").attr("src",foto);
+		 						$(".contenido").html(contenido);
+		 					})
+
+ 		}
+
+ 		function borrarFila(ele)
+ 		{
+ 			$(ele).parent().parent().remove();
+
+ 		}
